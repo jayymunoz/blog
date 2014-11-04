763de5510019c56d4c0ee3f0c9bbc318a5a74d54
justin munoz <jayymunoz66@gmail.com>
2014-11-04 07:59:07 -0800

    finished 1-16

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <script src="//ajax.googleapis.comlp/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <title></title>
-    </head>
-    <body>
-        <?php
-        $age='13';
-                
-       if($age >14){
-            echo 'you are older than 14.';
-       }
-       else if($age <13){
-           echo'you are not older than 15';
-       }
-       
-     
-       $soccerPlayers = array('Ronaldino' = 'goals', 'pele' => 'legend');
-       echo($soccerPlayers['ronaldino']);
-       echo "<br>";
-       print_r($cars);
-       
-        ?>
-        
-        
-    </body>
-</html>
+ <?php
+     require_once(__DIR__ . "/controller/create-db.php");
+ ?>
+   
