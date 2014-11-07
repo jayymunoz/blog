9ce440cd5e37775e4adcaf2c6787a137ed03d8c4
justin munoz <jayymunoz66@gmail.com>
2014-11-07 11:12:24 -0800

    create post file created

---
+++ controller/create-post.php
@@ -0,0 +1,13 @@
+<?php
+     $title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+     //this is a post where we send information
+     //this post is good to php
+     //this is so were able to filter the input
+     
+     
+     
+     echo "<p>Title : $title</p>";
+     echo "<p>Title : $post</p>";
+
+     //this folder is to create a post and to push it to our data base
