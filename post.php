038592d9fb1731fd883689a3e2ad329d5c297fd9
justin munoz <jayymunoz66@gmail.com>
2014-11-07 11:11:52 -0800

    created new files to data base

--- post.php
+++ post.php
@@ -3,3 +3,6 @@ require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/form.php");
 require_once(__DIR__ . "/view/footer.php");
 
+
+//telling the directory to make view files pop up /
+
--- view/header.php
+++ view/header.php
@@ -2,6 +2,10 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+        <link rel="stylesheet" type="text/css" href="main.css">`
         <title>Justin Blog </title>
     </head>
     <body>
+ 
